a44d48120f3f2595d9af631e590bfc63de9544a6
Adamyasood24 <soodadamya4@gmail.com>
2024-07-01 03:56:12 +0530

    model works with mnist images inverted or downloaded from the web which means the issue is in pre processing

--- apptest inp.py
+++ apptest inp.py
@@ -1,4 +1,3 @@
-# Function to preprocess the image
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -7,35 +6,27 @@ from tensorflow.keras.models import load_model
 model = load_model('C:/Users/sooda/Desktop/dev/New folder/Mishka-tech-internship/mnist.h5')
 
 def preprocess_image(image_path):
-    # Read the image using OpenCV
+
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Apply Gaussian blur to the image
 
+    image = cv2.bitwise_not(image)
 
-    # Resize the image to 28x28 (MNIST input size)
-    image = cv2.resize(image, (28, 28))
+    image = cv2.resize(image, (28,28), interpolation=cv2.INTER_LINEAR)
 
-    #image = cv2.GaussianBlur(image, (5, 5), 0)
+    image = image / 255.0
 
-    # Normalize the image pixels to be between 0 and 1
-    #image = image / 255.0
-
-    # Reshape the image to match the model's input shape
     image = image.reshape((1, 784))
 
     return image
 
-# Get the image path from the user
 image_path = input("Enter the path to the image file: ")
 
-# Preprocess the image
+
 image = preprocess_image(image_path)
 
-# Make predictions using the model
 predictions = model.predict(image)
 
-# Get the predicted digit
 predicted_digit = np.argmax(predictions)
 
 print("Predicted digit:", predicted_digit)
